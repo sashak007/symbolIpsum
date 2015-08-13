290c67dac2ab943499b3980a8f0242d83bfdfd43
Natasha <natasha.sha.shaaa@gmail.com>
2015-08-12 23:45:43 -0400

    refactored unit test

--- test/test.js
+++ test/test.js
@@ -2,21 +2,11 @@
 
 var expect = require('chai').expect,
 		dictionary = require('../lib/dictionary'),
-		helper = require('../lib/helper'),
 		symbolIpsum = require('../index');
 
-describe('Symbol Ipsum',function(){
-
-	describe('generating words', function(){
-
-		// words
-		it('should generate random words if no parameter is passed',function(){
-
-			var result = symbolIpsum.words();
-
-			expect(result[0].length).to.equal(10);
-
-			if(result[1] === 'symbols'){
+var define = function(result,charType){
+		
+		if(result[1] === 'symbols'){
 
 				for(var i = 0; i < result.length; i++){
 
@@ -33,6 +23,47 @@ describe('Symbol Ipsum',function(){
 				}
 
 			}
+		};
+
+	var verifySentences = function(result,charType,min,max){
+
+		for(var i = 0; i < result.length; i++){
+
+			var words = result[i].split(' ');
+
+			expect(words.length).to.be.within(min,max);
+
+			define(result,charType);
+		}
+
+	},
+
+	verifyParagraph = function(result,charType,numOfSentences,min,max){
+
+		for(var i = 0; i < result.length; i++){
+
+			var sentences = result[i].split('. ');
+
+			expect(sentences.length).to.equal(numOfSentences);
+
+			verifySentences(sentences,charType,min,max);
+				
+			}
+
+	};
+
+
+describe('Symbol Ipsum',function(){
+
+	describe('generating words', function(){
+
+		it('should generate random words if no parameter is passed',function(){
+
+			var result = symbolIpsum.words();
+
+			expect(result[0].length).to.equal(10);
+
+			define(result[0],result[1]);
 
 		});
 
@@ -42,11 +73,7 @@ describe('Symbol Ipsum',function(){
 
 			expect(result[0].length).to.equal(20); 
 
-			for(var i = 0; i < result.length; i++){
-
-				expect(dictionary.accentedChars).to.contain(result[0][i]);
-			
-			}
+			define(result[0],result[1]);
 
 		});
 
@@ -56,191 +83,94 @@ describe('Symbol Ipsum',function(){
 
 			expect(result[0].length).to.equal(20); 
 
-			for(var i = 0; i < result.length; i++){
-
-				expect(dictionary.charEntities).to.contain(result[0][i]);
-			
-			}
+			define(result[0],result[1]);
 
 		});
 
-	}); // End of words describe
+	}); 
 	
 	describe('generating sentences', function(){
 
-		// sentences
-
 		it('should generate random sentences with minimum of five and max of ten if no parameter is passed',function(){
 
-			var result = symbolIpsum.sentences();
+			var min = 5,
+					max = 15,
+					result = symbolIpsum.sentences();
 			
 			expect(result[0].length).to.equal(1);
 
-			for(var i = 0; i < result[0].length; i++){
-
-				var words = result[0][i].split(' ');
-
-				expect(words.length).to.be.within(5,15);
-
-				if(result[1] === 'symbols'){
-
-					for(var i = 0; i < result.length; i++){
-
-						expect(dictionary.accentedChars).to.contain(words[i]);
-
-					}
-
-				} else if(result[1] === 'entities'){
-
-					for(var i = 0; i < result.length; i++){
-
-						expect(dictionary.charEntities).to.contain(words[i]);
-
-					}
-
-				}
-			}
+			verifySentences(result[0],result[1],min,max);
 
 		});
 		
 		it('should generate random sentences with symbols parameters passed',function(){
 
-			var result = symbolIpsum.sentences(7,14,2,'symbols');
+			var min = 7,
+					max = 14,
+					result = symbolIpsum.sentences(min,max,2,'symbols');
 
 			expect(result[0].length).to.equal(2);
 
-			for(var i = 0; i < result[0].length; i++){
-
-				var words = result[0][i].split(' ');
-
-				expect(words.length).to.be.within(7,14);
-
-				for(var j = 0; j < words.length; j++){
-
-					expect(dictionary.accentedChars).to.contain(words[j]);
-
-				}
-
-			}
+			verifySentences(result[0],result[1],min,max);
 
 		});
 
 		it('should generate random sentences with html entity encoding parameters passed',function(){
 
-			var result = symbolIpsum.sentences(7,14,2,'entities');
+			var min = 7,
+					max = 14,
+					result = symbolIpsum.sentences(min,max,2,'entities');
 
 			expect(result[0].length).to.equal(2);
 
-			for(var i = 0; i < result.length; i++){
-
-				var words = result[0][i].split(' ');
-
-				expect(words.length).to.be.within(7,14);
-
-				for(var j = 0; j < words.length; j++){
-
-					expect(dictionary.charEntities).to.contain(words[j]);
-
-				}
-
-			}
+			verifySentences(result[0],result[1],min,max);
 
 		});
 
 	});
 
 
-	describe.only('generating paragraphs', function(){
-
-			// paragraphs
+	describe('generating paragraphs', function(){
 
 		it('should generate random paragraphs with symbols if no parameter is passed',function(){
 
-			var result = symbolIpsum.paragraphs();
+			var min = 5,
+					max = 15,
+					numOfSentences = 4,
+					result = symbolIpsum.paragraphs();
 
 			expect(result[0].length).to.equal(2);
 
-			for(var i = 0; i < result.length; i++){
-
-				var sentences = result[0][i].split('. ');
-
-				expect(sentences.length).to.equal(4);
-
-				for(var j = 0; j < sentences.length; j++){
-
-					var words = sentences[j].split(' ');
-
-					expect(words.length).to.be.within(5,15);
-					
-				}
-
-			}
+			verifyParagraph(result[0],result[1],numOfSentences,min,max);
 
 		});
 
-		it.only('should generate random paragraphs with symbols with a parameter passed',function(){
+		it('should generate random paragraphs with symbols with a parameter passed',function(){
 
-			var result = symbolIpsum.paragraphs(7,14,5,3,'symbols');
-
-			console.log(result[0]);
+			var min = 7,
+					max = 14,
+					numOfSentences = 5,
+					result = symbolIpsum.paragraphs(min,max,numOfSentences,3,'symbols');
 
 			expect(result[0].length).to.equal(3);
 
-			for(var i = 0; i < result[0].length; i++){
-
-				var sentences = result[0][i].split('. ');
-
-				expect(sentences.length).to.equal(5);
-
-				for(var j = 0; j < sentences.length; j++){
-
-					var words = sentences[j].split(' ');
-
-					expect(words.length).to.be.within(7,14);
-
-					for(var k = 0; k < words.length; k++){
-
-						expect(dictionary.accentedChars).to.contain(words[k]);
-
-					}
-					
-				}
-
-			}
+			verifyParagraph(result[0],result[1],numOfSentences,min,max);
 
 		});
 
 		it('should generate random paragraphs with html entity encoding parameters passed',function(){
 
-			var result = symbolIpsum.paragraphs(7,14,5,3,'entities');
+			var min = 7,
+					max = 14,
+					numOfSentences = 5,
+					result = symbolIpsum.paragraphs(min,max,numOfSentences,3,'entities');
 
 			expect(result[0].length).to.equal(3);
 
-			for(var i = 0; i < result.length; i++){
-
-				var sentences = result[0][i].split('. ');
-
-				expect(sentences.length).to.equal(5);
-
-				for(var j = 0; j < sentences.length; j++){
-
-					var words = sentences[j].split(' ');
-
-					expect(words.length).to.be.within(7,14);
-
-					for(var k = 0; k < words.length; k++){
-
-						expect(dictionary.charEntities).to.contain(words[k]);
-
-					}
-					
-				}
-
-			}
+			verifyParagraph(result[0],result[1],numOfSentences,min,max);
 
 		});
 
 	});
-
 
 });
